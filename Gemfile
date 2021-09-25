f136c6c52fb4d529dc3fdf414502637fa3845c71
Petr <petr@kle.cz>
2021-09-25 17:14:57 +0200

    Gzip + brotli

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
 source 'https://rubygems.org'
 
-gem "middleman", "~> 4.3"
+gem "middleman", "~> 4.4"
 gem 'middleman-minify-html'
 gem "builder"
 gem "redcarpet"
 gem "middleman-syntax"
+gem "middleman-brotli"
+gem "brotli", "~> 0.4.0"
--- config.rb
+++ config.rb
@@ -4,7 +4,6 @@ set :images_dir, 'img'
 
 set :markdown_engine, :redcarpet
 set :markdown, :fenced_code_blocks => true, :smartypants => true
-page "/googledacab463a141d625.html", :directory_index => false
 
 configure :build do
 	activate :syntax
@@ -12,4 +11,6 @@ configure :build do
 	activate :minify_html
   activate :minify_css
   activate :asset_hash
+  activate :gzip
+  activate :brotli
 end
